d2ee704d6347f00f7fb3786d3b5c5239a3f88f18
Zafer Karakus <zafer.umut97@gmail.com>
2022-04-13 03:16:20 +0300

    Added delete all item

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get Element
 const form = document.querySelector("form");
 
 const input = document.querySelector("#addTask");
@@ -6,20 +7,50 @@ const btnDltAll = document.querySelector("#dltAll");
 
 const taskList = document.querySelector("#taskList");
 
+// Call Function
 eventListeners();
 
-
+//Function 
 function eventListeners(){
     form.addEventListener("submit", addNewItem);
+    btnDltAll.addEventListener("click",dltAll);
+    taskList.addEventListener("click",dltItem);
 }
 
+// Add new item
+function addNewItem(e){
 
-function addNewItem(){
-    
+    // Create li
     const li = document.createElement('li');
-    const text = document.createTextNode(input.value);
     li.className= 'list-group-item';
+    
+    // Create a
+    const a = document.createElement('a');
+    a.classList="delete-item float-right";
+    a.setAttribute("href","#");
+    a.innerHTML="X";
+    
+    // Create Text
+    const text = document.createTextNode(input.value);
+    
+    // Li Append 
+    li.appendChild(a);
     li.appendChild(text);
     taskList.appendChild(li);
+    e.preventDefault();
 }
 
+// Remove All Item
+function dltAll(e){
+    var i = 0;
+    while(i <= taskList.childElementCount){
+        taskList.children[0].remove();
+    }
+
+}
+
+function dltItem(e){
+
+
+    e.preventDefault();
+}
